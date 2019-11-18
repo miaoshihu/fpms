cbb9d02d49b3643526acf07542c3e71760508d28
miaoshihu <miaoshihu@gmail.com>
2019-11-18 09:24:41 +0800

    Update Good & Need

--- msapp/admin.py
+++ msapp/admin.py
@@ -5,7 +5,7 @@ from django.contrib import admin
 
 from django.contrib import admin
 
-from .models import User, Good
+from .models import User, Good, Need, City
 
 
 class GoodInLine(admin.StackedInline):
@@ -13,17 +13,40 @@ class GoodInLine(admin.StackedInline):
     extra = 1
 
 
+class NeedInLine(admin.StackedInline):
+    model = Need
+    extra = 1
+
+
+class CityAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'enabled']
+
+
 class UserAdmin(admin.ModelAdmin):
-    list_display = ['name', 'login_times', 'create_time']
-    inlines = [GoodInLine]
+    list_display = ['id', 'nickname', 'login_times', 'create_time', 'enabled', 'white']
+    readonly_fields = ['nickname', 'icon', 'desc', 'create_time', 'login_times']
+    list_editable = ['enabled']
+    inlines = [GoodInLine, NeedInLine]
+
+    list_filter = ('enabled', 'white', 'city')
 
 
 class GoodAdmin(admin.ModelAdmin):
-    list_display = ['name', 'user', 'price', 'viewtimes', 'create_time']
+    list_display = ['name', 'price', 'status', 'short_desc', 'view_times', 'create_time']
+    readonly_fields = ['user', 'name', 'price', 'short_desc', 'phone', 'image1', 'image2', 'image3', 'desc', 'address',
+                       'view_times', 'create_time', 'last_modify_time', 'city']
+
+
+class NeedAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price_min', 'price_max', 'status', 'short_desc', 'view_times', 'create_time']
+    readonly_fields = ['user', 'name', 'price_min', 'price_max', 'phone', 'image1', 'address', 'desc', 'short_desc',
+                       'view_times', 'create_time', 'last_modify_time', 'city']
 
 
 admin.site.register(User, UserAdmin)
 admin.site.register(Good, GoodAdmin)
+admin.site.register(Need, NeedAdmin)
+admin.site.register(City, CityAdmin)
 
-admin.site.site_header = u'365便民管理系统'
-admin.site.site_title = u'365便民'
+admin.site.site_header = u'本地旧货header'
+admin.site.site_title = u'本地旧货title'
--- msapp/models.py
+++ msapp/models.py
@@ -1,16 +1,38 @@
 from django.db import models
 
 
+class City(models.Model):
+
+    id = models.CharField(max_length=20, blank=False, default=None, unique=False, primary_key=True)
+    id.verbose_name = u'城市id'
+
+    name = models.CharField(max_length=10, blank=False, default=None, unique=False)
+    name.verbose_name = u'城市名'
+
+    enabled = models.BooleanField(default=False)
+    enabled.verbose_name = u'启用'
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = u'城市'
+        verbose_name_plural = u'城市'
+
+
 # Create your models here.
 class User(models.Model):
-    name = models.CharField(max_length=50, blank=False, default=None, unique=True)
-    name.verbose_name = u'用户名'
+    id = models.CharField(max_length=50, blank=False, default=None, unique=True, primary_key=True)
+    id.verbose_name = u'用户标识'
+
+    city = models.ForeignKey(City, on_delete=models.CASCADE, null=False, blank=False)
+    city.verbose_name = u'城市'
+
+    nickname = models.CharField(max_length=50, blank=False, default=None, unique=False)
+    nickname.verbose_name = u'昵称'
 
     icon = models.CharField(max_length=50, blank=True, default=None, unique=False)
     icon.verbose_name = u'用户头像'
-
-    phoneno = models.CharField(max_length=50, blank=True, default=None, unique=False)
-    phoneno.verbose_name = u'电话号码'
 
     login_times = models.PositiveSmallIntegerField(null=False, blank=False)
     login_times.verbose_name = u"访问次数"
@@ -18,48 +40,126 @@ class User(models.Model):
     create_time = models.DateTimeField(auto_now_add=True)
     create_time.verbose_name = u'创建时间'
 
-    last_modify_time = models.DateTimeField(auto_now=True)
-    last_modify_time.verbose_name = u'最后修改时间'
+    last_login_time = models.DateTimeField(auto_now=True)
+    last_login_time.verbose_name = u'最后登录时间'
 
     enabled = models.BooleanField(default=False)
-    enabled.verbose_name = u'黑名单'
+    enabled.verbose_name = u'启用'
+
+    white = models.BooleanField(default=True)
+    white.verbose_name = u'非黑名单'
 
     desc = models.CharField(max_length=100, blank=True, default=None, unique=False)
-    desc = u'描述'
+    desc = u'备注'
 
     def __str__(self):
-        return self.name
+        return "" + self.nickname + " : " + self.id
 
     class Meta:
-        verbose_name = u'用户'
-        verbose_name_plural = u'用户列表'
+        verbose_name = u'微信用户'
+        verbose_name_plural = u'微信用户'
 
 
 class Good(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=True, )
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False, )
     user.verbose_name = u'用户'
+
+    city = models.ForeignKey(City, on_delete=models.CASCADE, null=False, blank=False, )
+    city.verbose_name = u'城市'
 
     name = models.CharField(max_length=10, blank=False, default=None, unique=False)
     name.verbose_name = u'物品名称'
 
-    price = models.CharField(max_length=10, blank=False, default=u'面议', unique=True)
-    price.verbose_name = u'价格'
+    image1 = models.CharField(max_length=200, null=True, blank=True, default=None, unique=False)
+    image1.verbose_name = u'图片1'
 
-    viewtimes = models.PositiveIntegerField(null=False, blank=False, default=0)
-    viewtimes.verbose_name = u'浏览次数'
+    image2 = models.CharField(max_length=200, null=True, blank=True, default=None, unique=False)
+    image2.verbose_name = u'图片2'
 
-    create_time = models.DateTimeField(auto_now_add=True)
+    image3 = models.CharField(max_length=200, null=True, blank=True, default=None, unique=False)
+    image3.verbose_name = u'图片3'
+
+    status = models.IntegerField(null=False, blank=False, default=0)
+    status.verbose_name = u"状态"
+
+    price = models.PositiveIntegerField(null=False, blank=False, default=10)
+    price.verbose_name = u"价格"
+
+    short_desc = models.CharField(max_length=20, null=True, blank=True, default=None, unique=False)
+    short_desc.verbose_name = u'一句话描述'
+
+    desc = models.TextField(max_length=60, null=True, blank=True, default=None, unique=False)
+    desc.verbose_name = u'详细描述'
+
+    address = models.CharField(max_length=20, null=True, blank=True, default=None, unique=False)
+    address.verbose_name = u'地址'
+
+    phone = models.PositiveIntegerField(null=False, blank=False, default=0)
+    phone.verbose_name = u'联系电话'
+
+    view_times = models.PositiveIntegerField(null=False, blank=False, default=0)
+    view_times.verbose_name = u'浏览次数'
+
+    create_time = models.DateTimeField(null=True, auto_now_add=True)
     create_time.verbose_name = u'创建时间'
 
-    last_modify_time = models.DateTimeField(auto_now=True)
+    last_modify_time = models.DateTimeField(null=True, auto_now=True)
     last_modify_time.verbose_name = u'最后修改时间'
-
-    enabled = models.BooleanField(default=False)
-    enabled.verbose_name = u'审核通过'
 
     def __str__(self):
         return self.name + " " + str(self.price)
 
     class Meta:
-        verbose_name = u'二手物品'
-        verbose_name_plural = u'二手物品列表'
+        verbose_name = u'出售'
+        verbose_name_plural = u'出售'
+
+
+class Need(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False, )
+    user.verbose_name = u'用户'
+
+    city = models.ForeignKey(City, on_delete=models.CASCADE, null=False, blank=False, )
+    city.verbose_name = u'城市'
+
+    name = models.CharField(max_length=10, blank=False, default=None, unique=False)
+    name.verbose_name = u'物品名称'
+
+    image1 = models.CharField(max_length=200, null=True, blank=True, default=None, unique=False)
+    image1.verbose_name = u'图片1'
+
+    status = models.IntegerField(null=False, blank=False, default=0)
+    status.verbose_name = u"状态"
+
+    price_min = models.PositiveIntegerField(null=False, blank=False, default=10)
+    price_min.verbose_name = u"价格上限"
+
+    price_max = models.PositiveIntegerField(null=False, blank=False, default=10)
+    price_max.verbose_name = u"价格上限"
+
+    short_desc = models.CharField(max_length=20, null=True, blank=True, default=None, unique=False)
+    short_desc.verbose_name = u'一句话描述'
+
+    desc = models.TextField(max_length=60, null=True, blank=True, default=None, unique=False)
+    desc.verbose_name = u'详细描述'
+
+    address = models.CharField(max_length=20, null=True, blank=True, default=None, unique=False)
+    address.verbose_name = u'地址'
+
+    phone = models.IntegerField(null=False, blank=False, default=0)
+    phone.verbose_name = u'联系电话'
+
+    view_times = models.PositiveIntegerField(null=False, blank=False, default=0)
+    view_times.verbose_name = u'浏览次数'
+
+    create_time = models.DateTimeField(null=True, auto_now_add=True)
+    create_time.verbose_name = u'创建时间'
+
+    last_modify_time = models.DateTimeField(null=True, auto_now=True)
+    last_modify_time.verbose_name = u'最后修改时间'
+
+    def __str__(self):
+        return self.name + " " + str(self.price_min + "-" + self.price_max)
+
+    class Meta:
+        verbose_name = u'求购'
+        verbose_name_plural = u'求购'
